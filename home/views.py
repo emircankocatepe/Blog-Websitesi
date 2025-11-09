dbe81ea6a5e93a4f8c7c3dac527b3605ab0ce279
test kullanicisi <emircan123kocatepe@gmail.com>
2025-11-10 00:27:45 +0300

    third commit was made, other stuff added to database

--- home/views.py
+++ home/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated:
+
+        context = {
+            'isim' : 'emir',
+        }
+
+    else:
+        context = {
+            'isim' : 'misafir'
+        }
+    return render(request, 'home.html', context)
